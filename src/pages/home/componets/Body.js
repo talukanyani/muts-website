fc4cc2a2a76f7210deda1d87d36a61e35b13605b
Talukanyani Mutshaeni <tmutshaeni@hotmail.com>
2022-08-14 07:28:42 +0200

    Body completed🚀

--- src/pages/home/componets/Body.js
+++ src/pages/home/componets/Body.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './body.module.css'
+import twitter from './assets/icon-twitter.svg'
+import instagram from './assets/icon-instagram.svg'
 
 function Body(props) {
     return (
@@ -8,29 +10,41 @@ function Body(props) {
                 <div className={`${styles.container} ${styles.container_apps}`}>
                     <h1>Apps</h1>
                     <p>Explore apps engineered and developed for you.</p>
-                    <button>Show all apps</button>
+                    <button>
+                        <span>Show all apps</span>
+                    </button>
                 </div>
                 <div id="talu" className={`${styles.container} ${styles.container_contact}`}>
                     <h1>Contact</h1>
                     <p>Get in touch with related information.</p>
-                    <button>Contact us</button>
+                    <button>
+                        <span>Contact us</span>
+                    </button>
                 </div>
                 <div className={`${styles.container} ${styles.container_connect}`}>
                     <h1>Connect with us</h1>
-                    <p>Never miss updates, subscribe to our newsletter or follow us in our social medias</p>
+                    <p>Never miss updates, subscribe to our newsletter or follow us in our social media.</p>
                     <section>
                         <form>
-                            <input type='email' />
+                            <input type='email' placeholder='Email' />
                             <input type='submit' value='Subscribe' />
                         </form>
                         <ul>
-                            <li>twitter</li>
-                            <li>instagram</li>
+                            <li>
+                                <a target='_blank' href='https://twitter.com/tmlab_dot_app'>
+                                    <img src={twitter} alt="twitter icon" />
+                                </a>
+                            </li>
+                            <li>
+                                <a target='_blank' href='https://instagram.com/tmlab.app'>
+                                    <img src={instagram} alt="instagram icon" />
+                                </a>
+                            </li>
                         </ul>
                     </section>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
 

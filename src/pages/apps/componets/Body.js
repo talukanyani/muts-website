b909610fe7ba75d3608f94079c86665d5c1e6f6e
Talukanyani Mutshaeni <tmutshaeni@hotmail.com>
2022-09-11 04:56:15 +0200

    App page restructured

--- src/pages/apps/componets/Body.js
+++ src/pages/apps/componets/Body.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './body.module.css'
+
 import SmallHeading from '../elements/SmallHeading'
+
 import stlogo from './assets/st-logo.svg'
 import appstore from './assets/appstore.svg'
 import googleplay from './assets/googleplay.svg'
@@ -12,40 +14,62 @@ function Body(props) {
             <div className={styles.body}>
                 <SmallHeading text='Apps' />
                 <div className={styles.container}>
-                    <h1>
-                        <img
-                            src={stlogo}
-                            alt="ST App logo"
-                        />
-                        Student Tables
-                    </h1>
-                    <p>Manage assessments and activities.</p>
-                    <ul>
-                        <li>
-                            <a href='/'>
-                                <img
-                                    src={appstore}
-                                    alt='downlod on app store'
-                                />
-                            </a>
-                        </li>
-                        <li>
-                            <a href='/'>
-                                <img
-                                    src={googleplay}
-                                    alt='downlod on google play'
-                                />
-                            </a>
-                        </li>
-                        <li>
-                            <a href='/'>
-                                <img
-                                    src={appgallery}
-                                    alt='downlod on app gallery'
-                                />
-                            </a>
-                        </li>
-                    </ul>
+                    <div className={styles.st_image}>
+
+                    </div>
+                    <div className={styles.st_content}>
+                        <div className={styles.apps_heading}>
+                            <img
+                                src={stlogo}
+                                alt="ST App logo"
+                            />
+                            <section>
+                                <h1>Student Tables</h1>
+                                <p>Manage assessments and activities.</p>
+                            </section>
+                        </div>
+                        <div>
+                            <ul>
+                                <li>
+                                    <a href='/'>
+                                        <img
+                                            src={appstore}
+                                            alt='downlod on app store'
+                                        />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/'>
+                                        <img
+                                            src={googleplay}
+                                            alt='downlod on google play'
+                                        />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='/'>
+                                        <img
+                                            src={appgallery}
+                                            alt='downlod on app gallery'
+                                        />
+                                    </a>
+                                </li>
+                            </ul>
+                            {/* <section>
+                                <p>
+                                    Student Tables is not yet available, leave
+                                    your emain below to get notified.
+                                </p>
+                                <form>
+                                    <input
+                                        type='email'
+                                        name='stemail'
+                                    />
+                                    <input type='submit' />
+                                </form>
+                            </section> */}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

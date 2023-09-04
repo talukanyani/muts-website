db2d07a0f589063d1dec1d44047237ce7ebf2d0b
Talukanyani Mutshaeni <tmutshaeni@hotmail.com>
2023-09-04 23:19:01 +0200

    Student Calendar page UI updates

--- src/pages/student_calendar/components/Body.js
+++ src/pages/student_calendar/components/Body.js
@@ -2,31 +2,48 @@ import React from 'react';
 import styles from './Body.module.css'
 import { createPortal } from 'react-dom';
 import { useHandlePortal } from '../../../hooks/useHandlePortal'
-import Description from './Description'
 import DownloadInfoModal from '../modals/DownloadInfo'
+
 import playStoreImage from '../../../assets/images/play_store.svg'
 import appStoreImage from '../../../assets/images/app_store.svg'
+import sc_home_screen from '../../../assets/images/sc_home_screen.png'
 
 export default function Body() {
     return (
         <div className={styles.body}>
-            <div className={styles.body_content}>
-                <header>
-                    <h1>Student Calendar</h1>
-                    <p className={styles.big_text}>
-                        Stay Organised and on Track.
-                        Manage your Academic Activities.
+            <header>
+                <h1>Student Calendar</h1>
+                <h2>
+                    Stay Organised and on Track.
+                    Manage your Academic Activities.
+                </h2>
+                <DownloadButtons />
+            </header>
+            <div className={styles.efficient_app}>
+                <section>
+                    <img
+                        alt='Student Calendar home screen'
+                        src={sc_home_screen}
+                    />
+                </section>
+                <section>
+                    <h2>Efficient App</h2>
+                    <h3>
+                        Developed to revolutionise the way students
+                        manage their assessments and activities.
+                    </h3>
+                    <p>
+                        Student Calendar simplifies the process of organising, tracking,
+                        and prioritising tasks.
+                        Seamlessly integrate your assessments, exams, project due dates,
+                        and extracurricular activities all in one place.
+                        Visualise your academic schedule in table, list and calendar views.
                     </p>
-                    <DownloadButtons />
-                </header>
-                <Description />
-                <div className={styles.download}>
-                    <h2>Download</h2>
-                    <p className={styles.big_text}>
-                        Unleash the power of your mobile device.
-                    </p>
-                    <DownloadButtons />
-                </div>
+                </section>
+            </div>
+            <div className={styles.download}>
+                <h2>Download</h2>
+                <DownloadButtons />
             </div>
         </div>
     );
